52772fb668d24adab8dfc73506f75640c214aabf
michal <m.losak@safistudio.pl>
2020-04-09 12:55:30 +0200

    categories

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,4 @@ include 'api/auth.api.php';
 include 'api/front.api.php';
 include 'api/user.api.php';
 
-Route::get('categories', 'Categories\CategoryController@items');
+Route::post('categories', 'Categories\CategoryController@items');
