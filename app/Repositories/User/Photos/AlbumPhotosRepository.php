f09a51d83d7d0383962f66a9fa2c965d7c0ab5ed
SoftSquad <biuro@softsquad.pl>
2020-06-12 02:52:46 +0200

    photos

--- app/Repositories/User/Photos/AlbumPhotosRepository.php
+++ app/Repositories/User/Photos/AlbumPhotosRepository.php
@@ -3,13 +3,15 @@
 namespace App\Repositories\User\Photos;
 
 use App\Models\Users\Photos\AlbumPhotos;
+use Illuminate\Support\Facades\Auth;
 
 class AlbumPhotosRepository
 {
 
     public function items(array $params)
     {
-        $items = AlbumPhotos::orderBy('id', $params['order_by'] ?? 'DESC');
+        $items = AlbumPhotos::orderBy('id', $params['order_by'] ?? 'DESC')
+            ->where('user_id', Auth::id());
         if (!empty($params['name']))
             $items->where('name', 'like', '%' . $params['name'] . '%');
 
